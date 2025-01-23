3249be199e351821f2fd16f55c80f8b1060b038d
MR-ABHSIHEK <abhisrp9@gmail.com>
2025-01-24 02:02:26 +0530

    Fixed employee management features and templates

--- EducationCrmProject/src/main/java/in/sh/main/repositories/EmployeeRepository.java
+++ EducationCrmProject/src/main/java/in/sh/main/repositories/EmployeeRepository.java
@@ -1,2 +1,10 @@
-package in.sh.main.repositories;public class EmployeeRepository {
+package in.sh.main.repositories;
+
+import in.sh.main.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Employee findByEmail(String employeeEmail);
 }
